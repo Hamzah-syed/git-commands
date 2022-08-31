42d962cb0178bac246b57411567ec28e4fc5680b
hamzah-syed <hamzah.syed17@gmail.com>
2022-08-31 23:42:55 +0500

    updated main.js

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-function saySomething(x) {
-
-  const promise = new Promise(resolve => {
-    setTimeout(() => {
-      resolve("something!!!!!!" + x);
-    }, 2000);
-  });
-
-  return promise
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName
+  this.birthYear = birthYear
+  // Never use this
+  // this.calcAge = function () {
+  //   console.log(2022 - birthYear)
+  // }
 }
 
-async function talk(x) {
-  const words = await saySomething(x);
-  console.log(words);
+
+
+Person.prototype.calcAge = function () {
+  // console.log(2037 - this.birthYear);
+  return 2037 - this.birthYear
 }
 
-talk(2);
-console.log("hello")
-talk(4);
-console.log("hello")
-talk(8);
+
+const hamzah = new Person("Hamzah", 2000)
+const areeb = new Person("Areeb", 2000)
+
+
+console.log(hamzah.__proto__)
+
+// console.log(areeb)
+
+  // 1. New {} is created
+  // 2. function is called, this = {}
+  // 3. {} linked to prototype
+  // 4. function automatically return object
