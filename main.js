a06c8d37c33ed037e17cb6b5f1c66f17e08400b5
hamzah-syed <hamzah.syed17@gmail.com>
2022-09-02 23:21:26 +0500

    oop file updated

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+
 const Person = function (firstName, birthYear) {
+  // console.log(this) // {}
   this.firstName = firstName
   this.birthYear = birthYear
+
+
+
   // Never use this
   // this.calcAge = function () {
   //   console.log(2022 - birthYear)
@@ -15,15 +20,46 @@ Person.prototype.calcAge = function () {
 }
 
 
+
+
 const hamzah = new Person("Hamzah", 2000)
 const areeb = new Person("Areeb", 2000)
+const arham = new Person("Arham", 2000)
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return object
 
 
-console.log(hamzah.__proto__)
+// console.log(hamzah)
+// console.log(hamzah.hasOwnProperty("calcAge"))
 
-// console.log(areeb)
 
-  // 1. New {} is created
-  // 2. function is called, this = {}
-  // 3. {} linked to prototype
-  // 4. function automatically return object
+
+
+// class expression
+// const PersonCl = class {
+//   constructor(firstName, birthYear) {
+//     this.firstName = firstName
+//     this.birthYear = birthYear
+//   }
+// }
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName
+    this.birthYear = birthYear
+  }
+
+  clacAge() {
+    console.log(2037 - this.birthYear);
+  }
+}
+
+
+const ali = new PersonCl("Ali", 2000)
+const hamzah2 = new PersonCl("Hamzah", 2000)
+
+console.log(ali)
+
